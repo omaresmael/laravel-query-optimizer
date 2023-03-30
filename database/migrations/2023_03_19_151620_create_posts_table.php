5bc26a1b2d9e4664f8accf6d19aeab27b0a6e99e
omaresmaeel <omaresmael590@gmail.com>
2023-03-30 22:55:33 +0200

    add test migrations

---
+++ database/migrations/2023_03_19_151620_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+
+        DB::table('posts')
+            ->insert(['title' => 'Awesome post', 'user_id' => 1]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
